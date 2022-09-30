841cb54f60eb7932b950ef815a957c2da3de5772
nicat <webdevelopernicat@gmail.com>
2022-09-30 22:42:33 +0400

    Menimsetme Operatorlari

---
+++ src/operatorlar/MainClassMenimsetmeOperatorlari.java
@@ -0,0 +1,21 @@
+package operatorlar;
+
+public class MainClassMenimsetmeOperatorlari {
+
+	public static void main(String[] args) {
+		int age=44;
+		System.out.println("age = "+age);
+		
+		age +=5;
+		System.out.println("age = "+age);
+		
+		age -=3;
+		System.out.println("age = "+age);
+		
+		age *=2;
+		System.out.println("age = "+age);
+		
+		age %=4;
+		System.out.println("age ="+age);
+	}
+}
