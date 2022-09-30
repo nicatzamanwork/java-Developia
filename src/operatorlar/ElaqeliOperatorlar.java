555d122e8ce1e972801f64ec75ff5e73c64b9d9f
nicat <webdevelopernicat@gmail.com>
2022-09-30 23:07:23 +0400

    Elaqeli Operatorlar

---
+++ src/operatorlar/ElaqeliOperatorlar.java
@@ -0,0 +1,18 @@
+package operatorlar;
+
+public class ElaqeliOperatorlar {
+
+	
+	public static void main(String[] args) {
+	int age =5;
+		boolean result = age !=5;
+		System.out.println(result);
+		if(result) {
+			System.out.println("yes");
+			
+		}else {
+			System.out.println("no");
+		}
+		
+	}
+}
