f12259d0e5cd8d8ddf17dfa01985cbe7f9c9b520
nicat <webdevelopernicat@gmail.com>
2022-09-30 22:51:58 +0400

    Sert Operatorlari

---
+++ src/operatorlar/SertOperatorlari.java
@@ -0,0 +1,20 @@
+package operatorlar;
+
+public class SertOperatorlari {
+	public static void main(String[] args) {
+		
+		int age = 12;
+		boolean canEnter = age >=18;
+		
+		if(age>=18) {
+			System.out.println("Enter");
+		}       
+		else {
+			System.out.println("Out");
+		}
+		
+		
+		
+	}
+
+}
